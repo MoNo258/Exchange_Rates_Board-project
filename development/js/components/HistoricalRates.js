3127be04ab2b85b855683623c8654394c5aa748b
Monika N <monika258@gmail.com>
2020-06-28 11:07:11 +0200

    hostorical rates - get arrays with rates and dates for chart

--- development/js/components/HistoricalRates.js
+++ development/js/components/HistoricalRates.js
@@ -1,22 +1,22 @@
 import React, {useState, useEffect} from "react";
 
 import {Card, CardBody, CardHeader, CardTitle, Col, Row, Table} from "reactstrap";
+import {element} from "prop-types";
 
 
 const HistoricalRates = () => {
-    const [historicalRates, setHistoricalRates] = useState([]);
+    const [historicalData, setHistoricalData] = useState([]);
     const [currentDate, setCurrentDate] = useState(new Date());
+    const [currencyTarget, setCurrencyTarget] = useState('');
     const [effectiveDate, setEffectiveDate] = useState([]);
 
     const currentYear = currentDate.getFullYear();
     const currentMonth = (currentDate.getMonth() + 1).toString().length === 2 ? currentDate.getMonth() + 1 : `0${currentDate.getMonth() + 1}`;
     const currentDay = currentDate.getDate().toString().length === 2 ? currentDate.getDate() : `0${currentDate.getDate()}`;
-    const newCurrentDate = `${currentYear}-${currentMonth}-${currentDay}`
+    const newCurrentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 
 
     useEffect(() => {
-
-
         fetch(`http://api.nbp.pl/api/exchangerates/rates/A/EUR/2020-01-01/${newCurrentDate}/`, {
             method: 'GET'
         })
@@ -26,27 +26,60 @@ const HistoricalRates = () => {
             })
             .then(data => {
                 console.log(data);
-                console.log(data.currency)
-                console.log(data.code)
-                console.log(data.rates)
-                console.log(data.rates[0].effectiveDate)
-                console.log(data.rates[0].mid)
-                console.log(data.rates[1].effectiveDate)
-                console.log(data.rates[1].mid)
-                setHistoricalRates(data);
-                setEffectiveDate(prev => [...prev, data.rates[0].effectiveDate])
-                console.log(effectiveDate)
+                setHistoricalData(data.rates);
+                setCurrencyTarget(data.code);
+            })
+            .then( () => {
+                console.log(historicalData);
+                console.log(currencyTarget);
+                console.log(historicalData[10].effectiveDate);
+                console.log(historicalData[10].mid);
+            })
+            .then( () => {
+                const histRatesArray = [];
+                const effDateArray = [];
+                historicalData.forEach( element => histRatesArray.push(element.mid));
+                console.log(histRatesArray);
+                historicalData.forEach( element => effDateArray.push(element.effectiveDate));
+                console.log(effDateArray);
+
             })
             .catch(error => console.log(error));
-    }, []);
+    }, [currencyTarget]);
+
+    // const myPromiseHistRates1 = new Promise( (resolve, reject) => {
+    //     try{
+    //         const result = a / b;
+    //         resolve(result);
+    //     } catch (error) {
+    //         reject(error)
+    //         // reject('dsads')
+    //     }
+    // });
+    //
+    // const myPromiseHistRates2 = new Promise( (resolve, reject) => {
+    //     if ( b !== 0){
+    //         const result = a / b;
+    //         resolve(result);
+    //     } else {
+    //         reject('blankswdeiow')
+    //     }
+    // });
+    //
+    // myPromiseHistRates1
+    //     .then( (result) => console.log(result))
+    //     .catch( (error) => console.log(error)); //przechwyci wartosc z reject
+    //
+    // myPromiseHistRates2
+    //     .then( (result) => console.log(result))
+    //     .catch( (error) => console.log(error));
 
 
 
-    return(
-        <>
-        <p></p>
-        </>
-    )
+
+
+
+    return
 
 };
 
