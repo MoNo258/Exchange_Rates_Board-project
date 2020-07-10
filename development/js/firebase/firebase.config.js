2f66f00168e8fcbf60bb7ee0032ff0e20eb32bc6
Monika N <monika258@gmail.com>
2020-07-10 14:54:35 +0200

    check if this working at all while using heroku config vars

--- development/js/firebase/firebase.config.js
+++ development/js/firebase/firebase.config.js
@@ -1,9 +1,10 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import {firebaseConfig} from "../services/secret";
+// import {firebaseConfig} from "../services/secret";
 
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(process.env.firebaseConfig);
 
 export default firebase;
