4a58c5089e4c0b603761549fac9aa97d8aee21e2
Monika N <monika258@gmail.com>
2020-07-10 14:21:36 +0200

    first try of using heroku config vars

--- development/js/services/constants.js
+++ development/js/services/constants.js
@@ -4,7 +4,7 @@ import {KEY_FIXER} from "../services/secret";
 export const API_URL_REST = 'http://data.fixer.io/api';
 export const latest_REST = '/latest'; //option for latest spots -> http://data.fixer.io/api/latest?access_key=YOUR_ACCESS_KEY&base=JPY&symbols=USD,AUD,CAD,PLN,MXN
 export const historical_REST = '2020-01-01'; ////option for historical rates/spots for given date -> http://data.fixer.io/api/2020-01-01?access_key=c02b87d109a928029679584b8d27fb65&base=EUR&symbols=USD,AUD,CAD,PLN,MXN
-export const API_KEY_REST = `?access_key=${KEY_FIXER}` || `?access_key=${process.env.KEY_FIXER}`;
+export const API_KEY_REST =  `?access_key=${process.env.KEY_FIXER}`;
 export const base_REST = '&base=EUR'; //optional //looks like only EUR is free. also EUR is default value (no need to provide this part at all)
 export const symbols_API_REST = 'USD,AUD,CAD'; //optional //quote currencies (against base)
 
