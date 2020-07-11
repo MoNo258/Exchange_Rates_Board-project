576a7aff06bff9d5ee224b38c555fbfda67d0a22
Monika N <monika258@gmail.com>
2020-07-12 00:42:53 +0200

    heroku try with express server.js

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-// const express = require('express');
-// const path = require('path');
-// const port = process.env.PORT || 8080;
-// const app = express();
-//
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname + '/build'));
-//
-// // send the user to index html page inspite of the url
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'build/index.html'));
-// });
-//
-// app.listen(port);
-
-
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
+const app = express();
 
-app.use('/', express.static(path.join(__dirname, 'build')));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname + '/build/'));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build/index.html'));
+});
 
 app.listen(port, () => console.log("Listening on Port", port));
+
+//
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const port = process.env.PORT || 3001;
+//
+// app.use('/', express.static(path.join(__dirname, 'build')));
+//
+// app.listen(port, () => console.log("Listening on Port", port));
