6affaecf13d3440096de692b512de3081d23977d
Monika N <monika258@gmail.com>
2020-07-11 18:52:50 +0200

    test: router with one component

--- development/js/router.js
+++ development/js/router.js
@@ -15,34 +15,37 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import Dashboard from "./views/Dashboard.js";
-import Charts from "./views/Charts.js";
+// import Dashboard from "./views/Dashboard.js";
+// import Charts from "./views/Charts.js";
 import Settings from "./views/Settings.js";
-import UserProfile from "./views/UserProfile.js";
-import News from "./views/News";
-import SignIn from "./views/SignIn";
-import Login from "./views/Login";
+// import UserProfile from "./views/UserProfile.js";
+// import News from "./views/News";
+// import SignIn from "./views/SignIn";
+// import Login from "./views/Login";
 
 var routes = [
     {
         path: "/dashboard",
         name: "Dashboard",
         icon: "tim-icons icon-chart-pie-36",
-        component: Dashboard,
+        // component: Dashboard,
+        component: Settings,
         layout: "/main"
     },
     {
         path: "/charts",
         name: "Charts",
         icon: "tim-icons icon-chart-bar-32",
-        component: Charts,
+        // component: Charts,
+        component: Settings,
         layout: "/main"
     },
     {
         path: "/news",
         name: "News",
         icon: "tim-icons icon-world",
-        component: News,
+        // component: News,
+        component: Settings,
         layout: "/main"
     },
     {
@@ -56,21 +59,24 @@ var routes = [
         path: "/sign-in",
         name: "Sign In",
         icon: "tim-icons icon-single-02",
-        component: SignIn,
+        // component: SignIn,
+        component: Settings,
         layout: "/main"
     },
     {
         path: "/login",
         name: "Login",
         icon: "tim-icons icon-single-02",
-        component: Login,
+        // component: Login,
+        component: Settings,
         layout: "/main"
     },
     {
         path: "/user-profile",
         name: "User Profile",
         icon: "tim-icons icon-single-02",
-        component: UserProfile,
+        component: Settings,
+        // component: UserProfile,
         layout: "/main"
     },
 
