5ee2b49a2c396a4f5f0a7ce9fc22b504793ede3c
Monika N <monika258@gmail.com>
2020-07-13 10:04:36 +0200

    firebase config changes

--- development/js/firebase/firebase.config.js
+++ development/js/firebase/firebase.config.js
@@ -1,5 +1,6 @@
 // import {firebaseConfigData} from "../services/constants";
-const firebaseConfigData = require("../services/constants");
+import firebaseConfigData from "../services/constants";
+// const firebaseConfigData = require("../services/constants");
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
